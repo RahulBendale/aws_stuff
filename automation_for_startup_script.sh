6df10c07466a3b40f405bed3d06b5be6a6f5b47b
RahulBendale <40611128+RahulBendale@users.noreply.github.com>
2024-01-22 07:15:31 +0530

    Create automation_for_startup_script.sh
    automation_for_startup_script.sh, This script can be added in the cm automation section to install any tools and software and make any services before your VM instance gets ready.
    
    It gives up the advantage of doing manual stuff in an automated way before VM boots up. so avoids all manual configurations.

---
+++ automation_for_startup_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y git wget
+
+#Install Docker
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -faSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint OEBFCD88
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu/ \
+    $ (lab_release -ca) \
+    stable
+
+apt-get update
+
+apt-get install -yq docker-ce
+cd /root
+git clone https://github.com/codespaces-io/codespaces.git
+curl -L "https://github.com/docker/compose/release/download/1.19.1/docker-compose-$(uname -a)-$(uname -m)" -o /user/local/bin/docker-compose
+chmod +x /user/local/bin/docker-compose
